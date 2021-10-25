c3bbd10860bce93a104ba2ae63941183c260572c
YuLili-git <72510006+YuLili-git@users.noreply.github.com>
2021-10-25 16:45:35 +0800

    Create 19. 24两两交换链表中的节点.py

---
+++ 19. 24两两交换链表中的节点.py
@@ -0,0 +1,29 @@
+#给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+#你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+#示例 1：
+#输入：head = [1,2,3,4]
+#输出：[2,1,4,3]
+#示例 2：
+#输入：head = []
+#输出：[]
+#示例 3：
+#输入：head = [1]
+#输出：[1]
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        res = ListNode(next = head)
+        pre = res
+        while pre.next and pre.next.next:
+            cur = pre.next
+            tmp = pre.next.next
+            cur.next = tmp.next
+            tmp.next = cur
+            pre.next = tmp
+
+            pre = pre.next.next
+        return res.next
