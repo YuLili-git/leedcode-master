b8c42bbc7f040d091a809298692aa647e6733f5b
YuLili-git <72510006+YuLili-git@users.noreply.github.com>
2021-11-05 10:20:39 +0800

    Update 49. 144二叉树的前序遍历.py

--- 49. 144二叉树的前序遍历.py
+++ 49. 144二叉树的前序遍历.py
@@ -54,4 +54,32 @@ class Solution:
             if root.left:
                 stack.append(root.left)
         return res
-            
+########################## solution 3 ##########################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        st = []
+        res = []
+        if root:
+            st.append(root)
+        while st:
+            node = st.pop()
+            if node != None:
+                
+                if node.right:
+                    st.append(node.right)
+                if node.left:
+                    st.append(node.left)
+                st.append(node)
+                st.append(None)
+            else:
+                node = st.pop()
+                res.append(node.val)
+        return res
+
+
