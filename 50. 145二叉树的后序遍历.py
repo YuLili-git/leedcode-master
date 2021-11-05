f4aac01f94a0c6c197710d4e081aa943c8383725
YuLili-git <72510006+YuLili-git@users.noreply.github.com>
2021-11-05 10:30:59 +0800

    Update 50. 145二叉树的后序遍历.py

--- 50. 145二叉树的后序遍历.py
+++ 50. 145二叉树的后序遍历.py
@@ -54,3 +54,33 @@ class Solution:
                 stack.append(root)
                 root = root.right
         return res
+
+    
+    
+############################ solution 3 ############################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        st = []
+        if root:
+            st.append(root)
+        while st:
+            node = st.pop()
+            if node != None:
+                st.append(node)
+                st.append(None)
+
+                if node.right:
+                    st.append(node.right)
+                if node.left:
+                    st.append(node.left)
+            else:
+                node = st.pop()
+                res.append(node.val)
+        return res
