91d72d44403ce4617f6bf8bb452b1c14865f2cf2
YuLili-git <72510006+YuLili-git@users.noreply.github.com>
2021-11-05 10:30:00 +0800

    Update 51. 94二叉树的中序遍历.py

--- 51. 94二叉树的中序遍历.py
+++ 51. 94二叉树的中序遍历.py
@@ -60,3 +60,32 @@ class Solution:
             res.append(root.val)
             root = root.right
         return res
+
+    
+
+#################### solution 3 ####################   
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        st = []
+        if root:
+            st.append(root)
+        while st:
+            node = st.pop()
+            if node != None:
+                if node.right:
+                    st.append(node.right)
+                st.append(node)
+                st.append(None)
+                if node.left:
+                    st.append(node.left)
+            else:
+                node = st.pop()
+                res.append(node.val)
+        return res
