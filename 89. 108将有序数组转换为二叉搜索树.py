604ccbe07ca067e7f8afc688e43e122309590fc3
YuLili-git <72510006+YuLili-git@users.noreply.github.com>
2021-11-09 10:03:16 +0800

    Create 89. 108将有序数组转换为二叉搜索树.py

---
+++ 89. 108将有序数组转换为二叉搜索树.py
@@ -0,0 +1,35 @@
+#给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+#高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+#示例 1：
+#输入：nums = [-10,-3,0,5,9]
+#输出：[0,-3,9,-10,null,5]
+#解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+#示例 2：
+#输入：nums = [1,3]
+#输出：[3,1]
+#解释：[1,3] 和 [3,1] 都是高度平衡二叉搜索树。
+#提示：
+#1 <= nums.length <= 104
+#-104 <= nums[i] <= 104
+#nums 按 严格递增 顺序排列
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        root = self.reverse(nums, 0, len(nums) - 1)
+        return root
+    def reverse(self, nums, left, right):
+        if left > right:
+            return None
+        mid_index = left + (right - left) // 2
+        root_val = nums[mid_index]
+        root = TreeNode(root_val)
+
+        root.left = self.reverse(nums, left, mid_index - 1)
+        root.right = self.reverse(nums, mid_index + 1, right)
+        
+        return root
